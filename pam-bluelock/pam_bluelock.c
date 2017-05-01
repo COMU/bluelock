e9fc6fe2d43955404aa20079a14018db1add16a9
Canberk Koç <canberkkoc@gmail.com>
2017-05-01 21:53:29 +0300

    Deleted unnecessary function

--- pam-bluelock/pam_bluelock.c
+++ pam-bluelock/pam_bluelock.c
@@ -51,35 +51,6 @@
 
 atomic_int bt_join_state = BT_JOIN_INACTIVE;
 
-static int find_conn(int s, int dev_id, long arg)
-{
-	struct hci_conn_list_req *cl;
-	struct hci_conn_info *ci;
-	int i;
-
-	if (!(cl = malloc(10 * sizeof(*ci) + sizeof(*cl)))) {
-		perror("Can't allocate memory");
-		exit(1);
-	}
-	cl->dev_id = dev_id;
-	cl->conn_num = 10;
-	ci = cl->conn_info;
-
-	if (ioctl(s, HCIGETCONNLIST, (void *) cl)) {
-		perror("Can't get connection list");
-		exit(1);
-	}
-
-	for (i = 0; i < cl->conn_num; i++, ci++)
-		if (!bacmp((bdaddr_t *) arg, &ci->bdaddr)) {
-			free(cl);
-			return 1;
-		}
-
-	free(cl);
-	return 0;
-}
-
 typedef struct {
     int *retval;
     pam_handle_t *pamh;
