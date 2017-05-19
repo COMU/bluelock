b5d7ebd7b76d72d3eca2826c63e798bfc0ab3b51
Çağrı ULAŞ <cagriulas@gmail.com>
2017-05-19 18:23:05 +0300

    changed build system to CMake in all projects

--- pam-bluelock/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#/lib/security
-set -e
-
-echo "pam-bluelock"
-cc -pthread -fPIC -fno-stack-protector -c pam_bluelock.c
-ld -x --shared -lbluetooth -lcrypt -o ./pam_bluelock.so pam_bluelock.o ../blueconf/blueconf.o ../blueconf/lex.yy.o ../blueconf/blueparse.tab.o
